aeca9e45317d7f1fcf50e2948bf7d81bca604149
ArsalanKm <arsalankarimzadeh1@gmail.com>
2022-01-21 16:08:36 +0330

    complex-client & complex-server & complex-worker container created

--- complex-fibonachi/client/src/App.js
+++ complex-fibonachi/client/src/App.js
@@ -1,22 +1,22 @@
 import logo from "./logo.svg";
 import { Link, Route, BrowserRouter as Router } from "react-router-dom";
-import OtherPage from "../otherPage";
-import Fib from "../Fib";
+import OtherPage from "./otherPage";
+import Fib from "./Fib";
 import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Link to="/">Home</Link>
-        <Link to="/otherpage">otherpage</Link>
-      </header>
-      <Router>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">otherpage</Link>
+        </header>
         <Route path="/" component={Fib} />
         <Route path="/otherpage" component={OtherPage} />
-      </Router>
-    </div>
+      </div>
+    </Router>
   );
 }
 
